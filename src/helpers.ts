5d338b1e8de4196726b49e0901ff1e3d5c8f6c8e
“Haoxu” <hzhan445@uottawa.ca>
2024-10-26 16:19:54 -0400

    fixed issue with gateway middleware

--- src/helpers.ts
+++ src/helpers.ts
@@ -4,7 +4,7 @@ export function firstLetterUppercase(str: string): string {
         .split(' ')
         .map(
             (value: string) =>
-            `${value.charAt(0).toUpperCase()}${value.slice(1).toLowerCase()}`
+                `${value.charAt(0).toUpperCase()}${value.slice(1).toLowerCase()}`
         )
         .join(' ');
 }
@@ -16,15 +16,15 @@ export function lowerCase(str: string): string {
 export const toUpperCase = (str: string): string => {
     return str ? str.toUpperCase() : str;
 };
-
+// CHECK VALID EMAIL
 export function isEmail(email: string): boolean {
     const regexExp =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
     return regexExp.test(email);
 }
 
 export function isDataURL(value: string): boolean {
     const dataUrlRegex =
-    /^\s*data:([a-z]+\/[a-z0-9-+.]+(;[a-z-]+=[a-z0-9-]+)?)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\\/?%\s]*)\s*$/i;
+        /^\s*data:([a-z]+\/[a-z0-9-+.]+(;[a-z-]+=[a-z0-9-]+)?)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\\/?%\s]*)\s*$/i;
     return dataUrlRegex.test(value);
 }
