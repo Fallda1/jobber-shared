d1333e8f1f756ea571658fbec6f56613046a8d0a
“Haoxu” <hzhan445@uottawa.ca>
2024-11-05 17:23:40 -0500

    fix possible issue2 with gateway

--- src/cloudinary_upload.ts
+++ src/cloudinary_upload.ts
@@ -1,26 +1,31 @@
-import cloudinary, { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary'
-
+import cloudinary, { UploadApiErrorResponse, UploadApiResponse } from 'cloudinary';
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+});
 export function uploads(
     file: string,
     public_id?: string,
     overwrite?: boolean,
     invalidate?: boolean
-): Promise<UploadApiErrorResponse | UploadApiResponse | undefined> {
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
+    console.log('Current Cloudinary Config Before Upload:', cloudinary.v2.config());
     return new Promise((resolve) => {
         cloudinary.v2.uploader.upload(
-            file, {
-            public_id,
-            overwrite,
-            invalidate,
-            resource_type: 'auto' //zip, images
-        },
+            file,
+            {
+                public_id,
+                overwrite,
+                invalidate,
+                resource_type: 'auto' // zip, images
+            },
             (error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
                 if (error) resolve(error);
                 resolve(result);
             }
         );
-
-    })
+    });
 }
 
 export function videoUpload(
@@ -28,21 +33,22 @@ export function videoUpload(
     public_id?: string,
     overwrite?: boolean,
     invalidate?: boolean
-): Promise<UploadApiErrorResponse | UploadApiResponse | undefined> {
+): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
+    console.log('Current Cloudinary Config Before Upload:', cloudinary.v2.config());
     return new Promise((resolve) => {
         cloudinary.v2.uploader.upload(
-            file, {
-            public_id,
-            overwrite,
-            invalidate,
-            chunk_size: 50000,
-            resource_type: 'video'
-        },
+            file,
+            {
+                public_id,
+                overwrite,
+                invalidate,
+                chunk_size: 50000,
+                resource_type: 'video'
+            },
             (error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
                 if (error) resolve(error);
                 resolve(result);
             }
         );
-
-    })
+    });
 }
